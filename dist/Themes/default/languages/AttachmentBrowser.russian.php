899aa221ad418091c523b8c3bacf211d469ea118
Bugo <bugo@dragomano.ru>
2022-04-11 13:48:46 +0500

    Add Russian translation

---
+++ dist/Themes/default/languages/AttachmentBrowser.russian.php
@@ -0,0 +1,49 @@
+<?php
+// Version: 2.1.1; AttachmentBrowser
+
+$txt['filename'] = 'Имя файла';
+$txt['poster_name'] = 'Автор';
+$txt['fileext'] = 'Расширение';
+$txt['fileext_long'] = 'Расширение';
+$txt['filesize'] = 'Размер файла';
+$txt['tags'] = 'Теги';
+$txt['submit'] = 'Отправить';
+$txt['return'] = 'Вернуться';
+
+$txt['attbr_list'] = 'Список вложений';
+$txt['attbr_browse'] = 'Обзор файлов';
+$txt['attbr_filter'] = 'Фильтр';
+$txt['attbr_search'] = 'Поиск вложений';
+$txt['attbr_editattachtags'] = 'Редактирование тегов вложения';
+$txt['attbr_desc'] = 'Выполнить обслуживание тегов, используемых в Attachment Browser.';
+
+$txt['attbr_tags'] = 'Теги вложений';
+$txt['attbr_tagmaint'] = 'Обслуживание тегов';
+
+$txt['attbr_add_tag'] = 'Добавление/изменение тега';
+$txt['attbr_category'] = 'Категория';
+$txt['attbr_category_desc'] = 'Логическая группа тегов (не более 25 символов)';
+$txt['attbr_tag'] = 'Тег';
+$txt['attbr_tag_desc'] = 'Теги являются буквенно-цифровыми и не должны содержать символов, зарезервированных для URL (максимум 25 символов).';
+$txt['attbr_aliases'] = 'Псевдонимы';
+$txt['attbr_aliases_desc'] = 'Список псевдонимов тегов через запятую (не более 50 символов)';
+
+$txt['attbr_delete_tag'] = 'Удалить тег';
+
+$txt['attbr_bad_tag'] = 'Неверное значение для тега';
+$txt['attbr_bad_aliases'] = 'Неверный список псевдонимов';
+$txt['attbr_bad_cat'] = 'Неверное значение для категории';
+$txt['attbr_too_many'] = 'Выбрано слишком много тегов';
+$txt['attbr_too_long'] = 'Слишком много тегов для вложения %1$s';
+
+$txt['attbr_autotag'] = 'Автотегирование';
+$txt['attbr_autotag_desc'] = 'Эта функция автоматически добавит теги для всех вложений.';
+$txt['attbr_start'] = 'Запустить';
+$txt['attbr_resume'] = 'Возобновить';
+$txt['attbr_complete'] = 'Готово';
+$txt['attbr_continue'] = 'Продолжить';
+
+$txt['viewing_attachs'] = 'Просмотр вложений %1$s из %2$s';
+$txt['of_total_attachs'] = 'из %1$s';
+
+?>
