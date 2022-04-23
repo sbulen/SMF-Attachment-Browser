073e2957da5de4c662b52c229ca18bd75dcf671b
sbulen <bulens@pacbell.net>
2022-04-23 10:30:22 -0700

    Add dutch translation from @rjen

---
+++ dist/Themes/default/languages/AttachmentBrowser.dutch.php
@@ -0,0 +1,49 @@
+<?php
+// Version: 2.1.1; AttachmentBrowser
+
+$txt['filename'] = 'Bestandsnaam';
+$txt['poster_name'] = 'Geplaatst door';
+$txt['fileext'] = 'Ext';
+$txt['fileext_long'] = 'Extensie';
+$txt['filesize'] = 'Bestandsgrootte';
+$txt['tags'] = 'Zoektermen';
+$txt['submit'] = 'Opslaan';
+$txt['return'] = 'Terug';
+
+$txt['attbr_list'] = 'Bijlagenlijst';
+$txt['attbr_browse'] = 'Bekijk bestanden';
+$txt['attbr_filter'] = 'Filteren op';
+$txt['attbr_search'] = 'Zoeken naar bijlagen';
+$txt['attbr_editattachtags'] = 'Wijzig bijlage zoektermen';
+$txt['attbr_desc'] = 'Hier kun je de zoektermen beheren die gebruikt kunnen worden bij het behren van bijlagen.';
+
+$txt['attbr_tags'] = 'Bijlage zoektermen';
+$txt['attbr_tagmaint'] = 'Onderhoud van zoektermen';
+
+$txt['attbr_add_tag'] = 'Beheer/wijzig zoekterm';
+$txt['attbr_category'] = 'Categorie';
+$txt['attbr_category_desc'] = 'Logische groep van zoektermen (maximaal 25 karakters)';
+$txt['attbr_tag'] = 'Zoekterm';
+$txt['attbr_tag_desc'] = 'Zoektermen zijn alfanumeriek en mogen geen karakters bevatten die voor URLs gereserveerd zijn (maximaal 25 karakters)';
+$txt['attbr_aliases'] = 'Aliassen';
+$txt['attbr_aliases_desc'] = 'Kommagescheiden lijst van zoekterm aliassen (maximaal 50 karakters)';
+
+$txt['attbr_delete_tag'] = 'Verwijder zoekterm';
+
+$txt['attbr_bad_tag'] = 'Ongeldige waarde voor zoekterm';
+$txt['attbr_bad_aliases'] = 'Ongeldige alias lijst';
+$txt['attbr_bad_cat'] = 'Ongeldige waarde voor categorie';
+$txt['attbr_too_many'] = 'Teveel zoektermen geselecteerd';
+$txt['attbr_too_long'] = 'Teveel zoektermen voor bijlage %1$s';
+
+$txt['attbr_autotag'] = 'Automatisch kenmerken';
+$txt['attbr_autotag_desc'] = 'Deze functie zal alle bijlagen automatische van zoektermen voorzien.';
+$txt['attbr_start'] = 'Start';
+$txt['attbr_resume'] = 'Verder gaan';
+$txt['attbr_complete'] = 'Afronden';
+$txt['attbr_continue'] = 'Doorgaan';
+
+$txt['viewing_attachs'] = 'Bekijkt bijlagen %1$s tot %2$s';
+$txt['of_total_attachs'] = 'van in totaal %1$s bijlagen';
+
+?>
--- dist/readme.txt
+++ dist/readme.txt
@@ -23,4 +23,5 @@ A "Search Again" button is provided to allow the user to refine the filter.
  - Performance is a concern given the flexibility provided.  This mod may not be appropriate for extremely large forums.  In testing, for forums with less than 20K attachments, performance is sub-second.  For a forum with 500K attachments, performance is ~2-6 seconds per page. YMMV. The more filters used to narrow the result set, the better!
 
 
-[b]Thanks to @Bugo for the Russian translation![/b]
+[b]Thanks to Bugo for the Russian translation![/b]
+[b]Thanks to @rjen for the Dutch translation![/b]
