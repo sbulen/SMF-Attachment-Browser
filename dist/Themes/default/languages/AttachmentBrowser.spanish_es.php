5c17a313cc489951397f065705bf9dd2b1ae7864
sbulen <bulens@pacbell.net>
2022-05-09 16:13:04 -0700

    Version 1.7 - Add Spanish translations

---
+++ dist/Themes/default/languages/AttachmentBrowser.spanish_es.php
@@ -0,0 +1,49 @@
+<?php
+// Version: 2.1.1; AttachmentBrowser
+
+$txt['filename'] = 'Nombre del archivo';
+$txt['poster_name'] = 'Publicado por';
+$txt['fileext'] = 'Ext';
+$txt['fileext_long'] = 'Extensión';
+$txt['filesize'] = 'Tamaño del archivo';
+$txt['tags'] = 'Etiquetas';
+$txt['submit'] = 'Enviar';
+$txt['return'] = 'Regresar';
+
+$txt['attbr_list'] = 'Lista de archivos adjuntos';
+$txt['attbr_browse'] = 'Búsqueda de archivos';
+$txt['attbr_filter'] = 'Filtrado por';
+$txt['attbr_search'] = 'Buscar archivos adjuntos';
+$txt['attbr_editattachtags'] = 'Editar etiquetas de archivos adjuntos';
+$txt['attbr_desc'] = 'Realizar el mantenimiento de las etiquetas utilizadas en el explorador de archivos adjuntos.';
+
+$txt['attbr_tags'] = 'Etiquetas de archivos adjuntos';
+$txt['attbr_tagmaint'] = 'Mantenimiento de etiquetas';
+
+$txt['attbr_add_tag'] = 'Agregar/Modificar Etiqueta';
+$txt['attbr_category'] = 'Categoría';
+$txt['attbr_category_desc'] = 'Grupo lógico de etiquetas (25 caracteres como máximo)';
+$txt['attbr_tag'] = 'Etiqueta';
+$txt['attbr_tag_desc'] = 'Las etiquetas son alfanuméricas y no deben contener caracteres reservados para URL (25 caracteres como máximo)';
+$txt['attbr_aliases'] = 'Alias';
+$txt['attbr_aliases_desc'] = 'Lista separada por comas de alias de etiquetas (50 caracteres como máximo)';
+
+$txt['attbr_delete_tag'] = 'Borrar Etiqueta';
+
+$txt['attbr_bad_tag'] = 'Valor no válido para la etiqueta';
+$txt['attbr_bad_aliases'] = 'Lista de alias no válido';
+$txt['attbr_bad_cat'] = 'Valor no válido para la categoría';
+$txt['attbr_too_many'] = 'Demasiadas etiquetas seleccionadas';
+$txt['attbr_too_long'] = 'Demasiadas etiquetas para el archivo adjunto %1$s';
+
+$txt['attbr_autotag'] = 'Etiqueta automática';
+$txt['attbr_autotag_desc'] = 'Esta función etiquetará automáticamente todos los archivos adjuntos.';
+$txt['attbr_start'] = 'Comenzar';
+$txt['attbr_resume'] = 'Resumen';
+$txt['attbr_complete'] = 'Completo';
+$txt['attbr_continue'] = 'Continuar';
+
+$txt['viewing_attachs'] = 'Visualizando archivos adjuntos %1$s a %2$s';
+$txt['of_total_attachs'] = 'de %1$s archivos adjuntos totales';
+
+?>
