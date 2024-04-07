9e4c4e14878de6bcceacaa624678026b644c3cef
sbulen <bulens@pacbell.net>
2024-04-07 09:30:11 -0700

    Add French translation

---
+++ dist/Themes/default/languages/AttachmentBrowser.french.php
@@ -0,0 +1,49 @@
+<?php
+// Version: 2.1.1; AttachmentBrowser
+
+$txt['filename'] = 'Nom du fichier';
+$txt['poster_name'] = 'Posté par';
+$txt['fileext'] = 'Ext';
+$txt['fileext_long'] = 'Extension';
+$txt['filesize'] = 'Taille du fichier';
+$txt['tags'] = 'Mots clé';
+$txt['submit'] = 'Soumettre';
+$txt['return'] = 'Return';
+
+$txt['attbr_list'] = 'Liste des pièces jointes';
+$txt['attbr_browse'] = 'Parcourir les fichiers';
+$txt['attbr_filter'] = 'Filtrer par';
+$txt['attbr_search'] = 'Rechercher des pièces jointes';
+$txt['attbr_editattachtags'] = 'Modifier les balises de pièce jointe';
+$txt['attbr_desc'] = 'Effectuer la maintenance des balises utilisées dans le navigateur de pièces jointes.';
+
+$txt['attbr_tags'] = 'Mots clé de pièce jointe';
+$txt['attbr_tagmaint'] = 'Maintenance des balises';
+
+$txt['attbr_add_tag'] = 'Ajouter/Modifier unmot clé';
+$txt['attbr_category'] = 'Catégorie';
+$txt['attbr_category_desc'] = 'Groupe logique de mots clé (25 caractères max)';
+$txt['attbr_tag'] = 'mot clé';
+$txt['attbr_tag_desc'] = 'Les balises sont alphanumériques et ne doivent pas contenir de caractères réservés aux URL (25 caractères maximum)';
+$txt['attbr_aliases'] = 'Aliases';
+$txt['attbr_aliases_desc'] = 'Liste d’alias de balises séparées par des virgules (50 caractères maximum)';
+
+$txt['attbr_delete_tag'] = 'Supprimer la balise';
+
+$txt['attbr_bad_tag'] = 'Valeur non valide pour la balise';
+$txt['attbr_bad_aliases'] = 'Liste d’alias non valide';
+$txt['attbr_bad_cat'] = 'Valeur non valide pour la catégorie';
+$txt['attbr_too_many'] = 'Trop de balises sélectionnées';
+$txt['attbr_too_long'] = 'Trop de balises pour la pièce jointe %1$s';
+
+$txt['attbr_autotag'] = 'Balisage automatique';
+$txt['attbr_autotag_desc'] = 'Cette fonction marquera automatiquement toutes les pièces jointes.';
+$txt['attbr_start'] = 'Commencer';
+$txt['attbr_resume'] = 'Reprendre';
+$txt['attbr_complete'] = 'Complet';
+$txt['attbr_continue'] = 'Continuer';
+
+$txt['viewing_attachs'] = 'Affichage des pièces jointes %1$s à %2$s';
+$txt['of_total_attachs'] = 'du total des pièces jointes %1$s';
+
+?>
